285e99eca1e83c233e389701fdd6fb5794f03aba
Bruno Costa <bcostinha01@gmail.com>
2023-02-06 21:40:31 +0000

    Create TaskManagement component, create interfaces folder

---
+++ my-app/src/components/TaskManagement.vue
@@ -0,0 +1,9 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default class TaskManagement extends Vue {}
+</script>
---
+++ my-app/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <TaskManagement />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import TaskManagement from "@/components/TaskManagement.vue";
+
+@Component({
+  components: {
+    TaskManagement,
+  },
+})
+export default class HomeView extends Vue {}
+</script>
